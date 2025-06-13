43011cbf3d7171c85e05723ad21e7cb79b7dcf33
Bosseyang <bosse_yang@hotmail.com>
2025-06-13 14:25:50 +0200

    Added Car Subtype to Vehicle

---
+++ Task5_Garage/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_Garage.Models
+{
+    public class Car : Vehicle
+    {
+
+        public string FuelType { get; }
+        public Car(string regNr, string color, string wheels, string fuelType) 
+            : base(regNr, color, wheels)
+        {
+            FuelType = fuelType;
+        }
+
+    }
+}
