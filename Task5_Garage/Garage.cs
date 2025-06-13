6a2d68e076eb485fad7f0abed5e35a3925c5005c
Bosseyang <bosse_yang@hotmail.com>
2025-06-13 14:15:37 +0200

    Updated Garage to be able to store vehicles and count them, can also input Capacity to Garage now.

--- Task5_Garage/Garage.cs
+++ Task5_Garage/Garage.cs
@@ -1,11 +1,34 @@
 ﻿using System.Collections;
 using Task5_Garage.Interfaces;
+using Task5_Garage.Models;
 
 namespace Task5_Garage
 {
-    public class Garage
+    public class Garage<T> : IEnumerable<T> where T : IVehicle
     {
+        private T[] vehicles;
+        private int count;
 
+        public int Capacity { get; }
+
+        public Garage(int capacity) 
+        {
+            Capacity = capacity;
+            vehicles = new T[capacity];
+            count = 0;
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = 0; i< count; i++)
+                yield return vehicles[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 
 }
