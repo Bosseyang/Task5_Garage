099fc5cae00355e88c33b6e01177b9874a872582
Bosseyang <bosse_yang@hotmail.com>
2025-06-17 15:43:55 +0200

    RandomPopulateGarage now works properly.

--- Task5_Garage/GarageHandler.cs
+++ Task5_Garage/GarageHandler.cs
@@ -6,11 +6,14 @@ namespace Task5_Garage
     public class GarageHandler : IHandler
     {
         private Garage<IVehicle> garage;
+        private UI ui;
         public int spot { get; }
+
 
         public GarageHandler(int capacity)
         {
             garage = new Garage<IVehicle>(capacity);
+            ui = new UI();
 
         }
         public bool CheckIfFull()
@@ -40,36 +43,69 @@ namespace Task5_Garage
                 Console.WriteLine(v.GetVehicleInfo());
         }
 
-        public bool RemoveVehicle(string registrationNumber)
+        public bool RemoveVehicle(string regNr)
         {
-            if (garage.RemoveVehicle(registrationNumber))
+            if (garage.RemoveVehicle(regNr))
             {
-                Console.WriteLine($"Vehicle with registration number: {registrationNumber} successfully removed");
+                Console.WriteLine($"Vehicle with registration number: {regNr} successfully removed");
                 return true;
             }
             else
             {
-                Console.WriteLine($"Vehicle with registration number: {registrationNumber} not found");
+                Console.WriteLine($"Vehicle with registration number: {regNr} not found");
                 return false;
             }
         }
-
         public void RandomPopulateGarage(int capacity)
         {
             var random = new Random();
             var colors = new[] { "Red   ", "Orange", "Yellow", "Green ", "Blue  ", "Black ", "White ", "Gray  " };
             var numberWheels = new[] { "3", "4", "6", "8" };
-            var fuelType = new[] { "Diesel", "Gasoline", "Ethanol", "Electric" };
+            var fuelTypes = new[] { "Diesel", "Gasoline", "Ethanol", "Electric" };
+            //var vehicleTypes = new[] { Enumerable.Range(1, 5).ToString() };
+            //var wingspans = new[] { Enumerable.Range(10, 40).ToString() };
+            //var lengths = new[] { Enumerable.Range(5, 40).ToString() };
+            //var cylinderVolumes = new[] {Enumerable.Range(49, 1500).ToString() };
+            //var numberOfSeats = new[] { Enumerable.Range(6, 80).ToString() };
 
             for (int i = 0; i < capacity; i++)
             {
                 if (CheckIfFull())
                     break;
-                string color = colors[random.Next(colors.Length)];
                 string regNr = RandomRegNr(random);
+                string color = colors[random.Next(colors.Length)];
                 string wheels = numberWheels[random.Next(numberWheels.Length)];
-                
-                string fuel = fuelType[random.Next(fuelType.Length)];
+                //Might have to fix potential Null here
+                string randomVehicleType = random.Next(1, 5).ToString()!;
+
+                switch (randomVehicleType)
+                {
+                    case "1":
+                        string wingspan = random.Next(10, 40).ToString();
+                        IVehicle airplane = new Airplane(regNr, color, wheels, wingspan);
+                        ParkVehicle(airplane);
+                        break;
+                    case "2":
+                        string length = random.Next(5, 40).ToString();
+                        IVehicle boat = new Boat(regNr, color, wheels, length);
+                        ParkVehicle(boat);
+                        break;
+                    case "3":
+                        string numberOfSeat = random.Next(8, 80).ToString();
+                        IVehicle buss = new Buss(regNr, color, wheels, numberOfSeat);
+                        ParkVehicle(buss);
+                        break;
+                    case "4":
+                        string fuelType = fuelTypes[random.Next(fuelTypes.Length)];
+                        IVehicle car = new Car(regNr, color, wheels, fuelType);
+                        ParkVehicle(car);
+                        break;
+                    case "5":
+                        string cylinderVolume = random.Next(50, 1500).ToString();
+                        IVehicle motorcycle = new Motorcycle(regNr, color, wheels, cylinderVolume);
+                        ParkVehicle(motorcycle);
+                        break;
+                }
 
                 //TODO: Add more vehicles as we go
                 //IVehicle vehicle = new Car("abc123", "Red", "4", "Diesel");
@@ -77,18 +113,15 @@ namespace Task5_Garage
                 //if (typeof(T) == typeof(Car))
                 //    vehicle = new Car(regNr, color, wheels, fuel);
                 //ParkVehicle((T)vehicle);
-                IVehicle vehicle = new Car(regNr, color, wheels, fuel);
-                ParkVehicle(vehicle);
-
             }
 
         }
         private string RandomRegNr(Random random)
         {
             string letters = new string(Enumerable.Range(0, 3).Select(r => (char)random.Next('A', 'Z' + 1)).ToArray());
+            //(100, 1000) makes it within range of three digits.
             string numbers = random.Next(100, 1000).ToString();
             return letters + numbers;
         }
-
     }
 }
