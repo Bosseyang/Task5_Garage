4f4dc7650d763bdf209b5a054c5ac1df0b3ea0c3
Bosseyang <bosse_yang@hotmail.com>
2025-06-23 10:56:39 +0200

    Added Test project with test for Garage

---
+++ GarageTest/GarageTest.cs
@@ -0,0 +1,168 @@
+﻿using Task5_Garage;
+using Task5_Garage.Interfaces;
+
+namespace GarageTest
+{
+    public class GarageTest
+    {
+        [Fact]
+        public void ParkVehicle_AddsVehicleIfGarageNotFull()
+        {
+            // Arrange
+            var garage = new Garage<IVehicle>(2);
+            var vehicle = new TestVehicle
+            {
+                RegistrationNumber = "abc123",
+                Color = "Red",
+                NumberOfWheels = 4
+
+            };
+            // Act
+            var result = garage.ParkVehicle(vehicle);
+
+            // Assert
+            Assert.Equal(1, result);
+            Assert.Equal(1, garage.Count);
+        }
+        [Fact]
+        public void ParkVehicle_ReturnsSpotIfValidVehicle()
+        {
+            var garage = new Garage<IVehicle>(2);
+            var vehicle = new TestVehicle
+            {
+                RegistrationNumber = "abc123",
+                Color = "Red",
+                NumberOfWheels = 4
+            };
+
+            int result = garage.ParkVehicle(vehicle);
+
+            Assert.Equal(1, result); // Should park at spot 1 (index 0 + 1)
+        }
+
+        [Fact]
+        public void ParkVehicle_Returns0IfGarageIsFull()
+        {
+            var garage = new Garage<IVehicle>(1);
+
+            var vehicle1 = new TestVehicle
+            {
+                RegistrationNumber = "abc123",
+                Color = "Red",
+                NumberOfWheels = 4
+
+            };
+            var vehicle2 = new TestVehicle
+            {
+                RegistrationNumber = "abc124",
+                Color = "Red",
+                NumberOfWheels = 4
+
+            };
+            garage.ParkVehicle(vehicle1);
+
+            var result = garage.ParkVehicle(vehicle2);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void ParkVehicle_ReturnsMinus1IfRegistrationNotUnique()
+        {
+            var garage = new Garage<IVehicle>(2);
+            var vehicle1 = new TestVehicle
+            {
+                RegistrationNumber = "abc123",
+                Color = "Red",
+                NumberOfWheels = 4
+
+            };
+            var vehicle2 = new TestVehicle
+            {
+                RegistrationNumber = "abc123",
+                Color = "Red",
+                NumberOfWheels = 4
+
+            };
+            garage.ParkVehicle(vehicle1);
+
+            var result = garage.ParkVehicle(vehicle2);
+
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void RemoveVehicle_ReturnsEmptyIfGarageIsEmpty()
+        {
+            var garage = new Garage<IVehicle>(2);
+
+            var result = garage.RemoveVehicle("aaa111");
+
+            Assert.Equal("Empty", result);
+        }
+
+        [Fact]
+        public void RemoveVehicle_ReturnsRemovedIfVehicleExists()
+        {
+            var garage = new Garage<IVehicle>(2);
+            var vehicle = new TestVehicle
+            {
+                RegistrationNumber = "abc123",
+                Color = "Red",
+                NumberOfWheels = 4
+
+            };
+            garage.ParkVehicle(vehicle);
+
+            var result = garage.RemoveVehicle("abc123");
+
+            Assert.Equal("Removed", result);
+            Assert.Equal(0, garage.Count);
+        }
+
+        [Fact]
+        public void Find_ReturnsCorrectVehicle()
+        {
+            var garage = new Garage<IVehicle>(2);
+            var vehicle = new TestVehicle
+            {
+                RegistrationNumber = "abc123",
+                Color = "Red",
+                NumberOfWheels = 4
+
+            }; 
+            garage.ParkVehicle(vehicle);
+
+            var found = garage.Find("abc123");
+
+            Assert.NotNull(found);
+            Assert.Equal("abc123", found!.RegistrationNumber);
+        }
+
+        [Fact]
+        public void CheckIfFull_ReturnsTrueIfGarageIsFull()
+        {
+            var garage = new Garage<IVehicle>(1);
+            var vehicle = new TestVehicle
+            {
+                RegistrationNumber = "abc123",
+                Color = "Red",
+                NumberOfWheels = 4
+
+            };
+
+            garage.ParkVehicle(vehicle);
+
+            Assert.True(garage.CheckIfFull());
+        }
+
+        [Fact]
+        public void CheckIfEmpty_ReturnsTrueIfGarageIsEmpty()
+        {
+            var garage = new Garage<IVehicle>(1);
+
+            Assert.True(garage.CheckIfEmpty());
+        }
+    }
+    
+}
---
+++ GarageTest/TestVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5_Garage.Interfaces;
+
+namespace GarageTest
+{
+    internal class TestVehicle : IVehicle
+    {
+        public string RegistrationNumber { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public int NumberOfWheels { get; set; } 
+
+        public string GetVehicleInfo()
+        {
+            return $"TestVehicle: {RegistrationNumber}, Color: {Color}, Wheels: {NumberOfWheels}";
+        }
+    }
+}
