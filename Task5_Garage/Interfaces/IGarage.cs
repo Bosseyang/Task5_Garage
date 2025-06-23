f8e407a4725c6e66ae334088b4a2fd8fa27bbfd2
Bosseyang <bosse_yang@hotmail.com>
2025-06-23 09:05:57 +0200

    Added Interface for Garage

---
+++ Task5_Garage/Interfaces/IGarage.cs
@@ -0,0 +1,15 @@
+﻿namespace Task5_Garage.Interfaces
+{
+    public interface IGarage<T> where T : IVehicle
+    {
+        int Capacity { get; }
+        int Count { get; }
+
+        bool CheckIfFull();
+        bool CheckIfEmpty();
+        T? Find(string registrationNumber);
+        IEnumerator<T> GetEnumerator();
+        int ParkVehicle(T vehicle);
+        string RemoveVehicle(string registrationNumber);
+    }
+}
